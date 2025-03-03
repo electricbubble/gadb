3225bdfdba2abeef406f896a7aa28da63c0b4991
Guo Tengda <guotengdaxx@gmail.com>
2025-03-03 10:08:14 +0800

    Create reader.go

---
+++ reader.go
@@ -0,0 +1,23 @@
+package gadb
+
+import (
+	"context"
+	"io"
+)
+
+type readerCtx struct {
+	ctx context.Context
+	r   io.Reader
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.r.Read(p)
+}
+
+// NewReader gets a context-aware io.Reader.
+func NewReader(ctx context.Context, r io.Reader) io.Reader {
+	return &readerCtx{ctx: ctx, r: r}
+}
